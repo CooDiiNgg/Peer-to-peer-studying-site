e0fa71429d0f666413c9c87debb673c0545b2289
CooDDiiNNgg <niki@valkanovi.com>
2022-12-15 08:13:13 +0200

    made functions for login and register

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,65 @@ const uri = "mongodb+srv://Mongo_db_base:Parola@peer-to-peer-studying-s.kjmhrnm.
 const dbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const sha1 = require("sha1")
 
+function login(username, password) {
+    try {
+        dbClient.connect((err) => {
+            const collection_users = dbClient.db("school").collection("users");
+            collection_users.find({ username: username }).toArray(function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                try{
+                    if (result[0].password === sha1(password)) {
+                        //res.send(`{"status": "success", "isTeacher": "${result[0].isTeacher}"}`);
+                        // session
+                        console.log(`{"status": "success", "isTeacher": "${result[0].isTeacher}"}`);
+
+                    }
+                    else {
+                        //res.send('{"status": "error", "msg": "Password is incorrect."}');
+                        console.log('{"status": "error", "msg": "Password is incorrect."}')
+                    }
+                }catch(err){
+                    //res.send('{"status": "error", "msg": "User does not exist."}');
+                    console.log('{"status": "error", "msg": "User does not exist."}')
+                }
+                
+                dbClient.close();
+            });
+
+        });
+    } catch (err) {
+        if (err) throw err;
+    }
+}
+
+function register(username, password, isTeacher, email){
+    try {
+        dbClient.connect((err) => {
+            let collection = dbClient.db("school").collection("users");
+            collection.find({username: username}).toArray((err, result) => {
+                if(err) throw err;
+                try{
+                    var check = result[0].password;
+                    //res.send('{"status": "error", "msg": "This user already exists."}');
+                    console.log('{"status": "error", "msg": "This user already exists."}');
+                }catch(err){
+                    collection.insertOne({ username: username, password: sha1(password), isTeacher: isTeacher, email: email }, function (err, res) {
+                        if (err) throw err;
+                        //res.send('{"status": "success"}');
+                        console.log('{"status": "success"}');
+                        dbClient.close();
+                    });
+                }
+            });
+            
+        });
+    } catch (err) {
+        throw err;
+    }
+}
+
 
 
 
@@ -15,37 +74,7 @@ app.post('/login', function (req, res) {
         full_data += data;
     }).on("end", () => {
         let user = JSON.parse(full_data);
-        try {
-            dbClient.connect((err) => {
-                const collection_users = dbClient.db("school").collection("users");
-                collection_users.find({ username: user.username }).toArray(function (err, result) {
-                    if (err) {
-                        throw err;
-                    }
-                    try{
-                        if (result[0].password === sha1(user.password)) {
-                            //res.send(`{"status": "success", "isTeacher": "${result[0].isTeacher}"}`);
-                            // session
-                            console.log(`{"status": "success", "isTeacher": "${result[0].isTeacher}"}`);
-    
-                        }
-                        else {
-                            //res.send('{"status": "error", "msg": "Password is incorrect."}');
-                            console.log('{"status": "error", "msg": "Password is incorrect."}')
-                        }
-                    }catch(err){
-                        //res.send('{"status": "error", "msg": "User does not exist."}');
-                        console.log('{"status": "error", "msg": "User does not exist."}')
-                    }
-                    
-                    dbClient.close();
-                });
-
-            });
-        } catch (err) {
-            if (err) throw err;
-        }
-
+        login(user.username, user.password);
         res.end();
         return;
     });
@@ -61,29 +90,7 @@ app.post('/register', function (req, res) {
         full_data += dat
     }).on("end", function () {
         let info = JSON.parse(full_data);
-        try {
-            dbClient.connect((err) => {
-                let collection = dbClient.db("school").collection("users");
-                collection.find({username: info.username}).toArray((err, result) => {
-                    if(err) throw err;
-                    try{
-                        var check = result[0].password;
-                        //res.send('{"status": "error", "msg": "This user already exists."}');
-                        console.log('{"status": "error", "msg": "This user already exists."}');
-                    }catch(err){
-                        collection.insertOne({ username: info.username, password: sha1(info.password), isTeacher: "0", email: "" }, function (err, res) {
-                            if (err) throw err;
-                            //res.send('{"status": "success"}');
-                            console.log('{"status": "success"}');
-                            dbClient.close();
-                        });
-                    }
-                });
-                
-            });
-        } catch (err) {
-            throw err;
-        }
+        register(info.username, info.password, "0", "");
         res.end()
         return;
     })
